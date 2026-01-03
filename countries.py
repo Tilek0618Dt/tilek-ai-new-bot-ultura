f15276a29f3ea6c22e36b00227010b386c956cb6
Tilek0618Dt <milardermilon@gmail.com>
2026-01-03 17:51:38 +0300

    Create countries.py

---
+++ countries.py
@@ -0,0 +1,14 @@
+# countries.py
+
+COUNTRIES = [
+    {"code": "KG", "name": "🇰🇬 Kyrgyzstan", "lang": "ky"}, 
+    {"code": "KZ", "name": "🇰🇿 Kazakhstan", "lang": "ru"},
+    {"code": "UZ", "name": "🇺🇿 Uzbekistan", "lang": "uz"},
+    {"code": "RU", "name": "🇷🇺 Russia", "lang": "ru"},
+    {"code": "TR", "name": "🇹🇷 Turkey", "lang": "tr"},
+    {"code": "US", "name": "🇺🇸 United States", "lang": "en"},
+    {"code": "DE", "name": "🇩🇪 Germany", "lang": "en"},
+    {"code": "FR", "name": "🇫🇷 France", "lang": "en"},
+    # ...
+    # бул жерге 350+ өлкө / аймак кошулат
+]
