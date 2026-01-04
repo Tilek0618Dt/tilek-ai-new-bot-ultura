b93c6270a5a0d373e9004686d4c2daa6da54e406
Tilek0618Dt <milardermilon@gmail.com>
2026-01-04 17:03:47 +0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,8 @@
+def voice_to_text():
+    return "🎙️ Voice DEMO → текстке айланды"
+
+def analyze_image():
+    return "📷 Сүрөт DEMO анализденди"
+
+def analyze_document():
+    return "📄 Документ DEMO окулду"
