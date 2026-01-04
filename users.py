81c65dab21ddc1d5e2f28a81abdd79f84b7a3cdf
Tilek0618Dt <milardermilon@gmail.com>
2026-01-04 17:48:35 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,15 +1,9 @@
-users = {}
+# users.py
+
+_users = {}
 
 def get_user(user_id):
-    return users.get(user_id)
+    return _users.get(user_id)
 
-def save_user(user_id, country, language, plan="free"):
-    users[user_id] = {
-        "country": country,
-        "language": language,
-        "plan": plan
-    }
-
-def set_plan(user_id, plan):
-    if user_id in users:
-        users[user_id]["plan"] = plan
+def save_user(user_id, data):
+    _users[user_id] = data
