5e30bceeb787ef693738599cfd8484ca51ce43ca
Tilek0618Dt <milardermilon@gmail.com>
2026-01-04 16:59:59 +0300

    Create languages.py

---
+++ languages.py
@@ -0,0 +1,8 @@
+COUNTRIES = {
+    "KG": {"name": "Кыргызстан", "lang": "ky", "flag": "🇰🇬"},
+    "RU": {"name": "Россия", "lang": "ru", "flag": "🇷🇺"},
+    "US": {"name": "United States", "lang": "en", "flag": "🇺🇸"},
+    "TR": {"name": "Türkiye", "lang": "tr", "flag": "🇹🇷"},
+    "KZ": {"name": "Kazakhstan", "lang": "kk", "flag": "🇰🇿"},
+    # кийин CSV/JSON менен 350+ толтурабыз
+}
