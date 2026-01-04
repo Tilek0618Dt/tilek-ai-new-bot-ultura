6cf001310c703b40ea224b2a518215974810fa4d
Tilek0618Dt <milardermilon@gmail.com>
2026-01-04 16:51:21 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "СЕНИН_BOT_TOKEN"
+
+DEFAULT_PLAN = "free"
+
+PLANS = {
+    "free": {"name": "FREE"},
+    "plus": {"name": "PLUS", "price": 8},
+    "pro": {"name": "PRO", "price": 18}
+}
