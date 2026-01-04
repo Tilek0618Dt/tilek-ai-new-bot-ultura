f10cfacf561141656d9323ed4aa1fa24675d0f90
Tilek0618Dt <milardermilon@gmail.com>
2026-01-04 16:56:03 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,41 +1,15 @@
-# users.py
-import json
-import os
-from threading import Lock
+users = {}
 
-FILE = "users.json"
-_lock = Lock()
+def get_user(user_id):
+    return users.get(user_id)
 
-def _load():
-    if not os.path.exists(FILE):
-        return {}
-    with open(FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
+def save_user(user_id, country, language, plan="free"):
+    users[user_id] = {
+        "country": country,
+        "language": language,
+        "plan": plan
+    }
 
-def _save(data):
-    with open(FILE, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-
-def get_user(user_id: int):
-    with _lock:
-        data = _load()
-        return data.get(str(user_id))
-
-def save_user(user_id: int, country: str, language: str, plan="free"):
-    with _lock:
-        data = _load()
-        data[str(user_id)] = {
-            "country": country,
-            "language": language,
-            "plan": plan
-        }
-        _save(data)
-
-def update_user(user_id: int, **fields):
-    with _lock:
-        data = _load()
-        uid = str(user_id)
-        if uid not in data:
-            return
-        data[uid].update(fields)
-        _save(data)
+def set_plan(user_id, plan):
+    if user_id in users:
+        users[user_id]["plan"] = plan
